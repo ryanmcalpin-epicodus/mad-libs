84870a97e4e739f9bb79b5e6d535fc55d74b818e
Evan Bishop and Ryan McAlpin <evan@>
2017-02-20 10:06:11 -0800

    add arrays to dry up code

--- js/madlib.js
+++ js/madlib.js
@@ -1,31 +1,37 @@
 $(document).ready(function() {
   $("#madlib form").submit(function() {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput= $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
+    //
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    var blanks = ['person1', 'person2', 'animal', 'exclamation', 'verb', 'noun'];
+
+    blanks.forEach(function(blank){
+      var  userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
 
     event.preventDefault();
   });
 
-  $("#self-letter form").submit(function() {
-    var username = $('#user-name').val();
+  // $("#self-letter form").submit(function() {
+  //   var username = $('#user-name').val();
+  //
+  //   $('.user-name-output').text(username);
+  //   $('#user-name').fadeOut();
+  //   $("#response-letter").slideDown();
 
-    $('.user-name-output').text(username);
-    $('#user-name').fadeOut();
-    $("#response-letter").slideDown();
-
-    event.preventDefault();
+    // event.preventDefault();
   });
-});
